af870ff8759620f52a65c4f413aa1850bc2e5043
Chris Bailey <chris@ghostly.io>
2021-11-19 20:32:09 -0500

    :art: opensea api endpoint cleanup

--- src/pages/api/os.ts
+++ src/pages/api/os.ts
@@ -13,51 +13,43 @@ export default async function handler(
 
   if (!wallet) throw new Error('Must provide wallet');
 
-  const dres = await fetch(
+  const collectionResponse = await fetch(
     `https://api.opensea.io/api/v1/collections?asset_owner=${wallet}&offset=0&limit=300`
   );
 
-  const data: Collection[] = await dres.json();
+  let collections: Collection[] = await collectionResponse.json();
 
-  const filledData: Collection[] = await Promise.all(
-    data.map(async (collection) => {
+  collections = await Promise.all(
+    collections.map(async (collection) => {
       const res = await fetch(
         `https://api.opensea.io/api/v1/collection/${collection.slug}/stats`
       );
       const stats = await res.json();
 
-      const newCollection = {
-        description: collection.description,
-        image_url: collection.image_url,
-        name: collection.name,
-        owned_asset_count: collection.owned_asset_count,
-        slug: collection.slug,
+      return {
+        ...collection,
         stats: { ...stats.stats },
       };
-
-      return newCollection;
     })
   );
 
-  let cleanedData = filledData;
-
   if (small === 'true') {
-    cleanedData = cleanedData.filter((data) => data.stats.floor_price >= 0.01);
+    collections = collections.filter((data) => data.stats.floor_price >= 0.01);
   }
 
   if (vol === 'true') {
-    cleanedData = cleanedData.filter((data) => data.stats.one_day_volume !== 0);
+    collections = collections.filter((data) => data.stats.one_day_volume !== 0);
   }
 
   if (sort === 'floor') {
-    cleanedData = cleanedData.sort((a, b) =>
+    collections = collections.sort((a, b) =>
       a.stats.floor_price < b.stats.floor_price ? 1 : -1
     );
   } else {
-    cleanedData = cleanedData.sort((a, b) =>
+    collections = collections.sort((a, b) =>
       a.stats.one_day_volume < b.stats.one_day_volume ? 1 : -1
     );
   }
 
-  res.status(200).json({ collections: cleanedData });
+  res.status(200).json({ collections: collections });
 }
