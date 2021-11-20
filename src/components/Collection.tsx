d5b80ed0c037b1e6a5186367c61c24fadfed62a0
Chris Bailey <chris@ghostly.io>
2021-11-20 18:17:27 -0500

    :sparkles: support sidebar
    add email link and tipping ui

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -12,12 +12,26 @@ export const CollectionCard = ({ collection }: Props) => {
       key={collection.slug}
     >
       <div className="flex-grow p-2">
-        {collection.name} ({collection.owned_asset_count})
+        <p className="flex items-center">
+          <img
+            src={collection.image_url}
+            height="30"
+            width="30"
+            className="mr-2 rounded border-2 border-black"
+          />
+          <a
+            href={`https://opensea.io/collection/${collection.slug}`}
+            className="mr-2 text-blue-700 underline hover:text-blue-800 hover:no-underline"
+          >
+            {collection.name}
+          </a>{' '}
+          ({collection.owned_asset_count})
+        </p>
       </div>
-      <div className="flex justify-center p-2 w-1/6 bg-blue-300 rounded-l">
+      <div className="flex justify-center p-2 w-1/5 bg-blue-300 rounded-l">
         F: {collection.stats.floor_price?.toFixed(2)}Ξ
       </div>
-      <div className="flex justify-center p-2 w-1/6 bg-green-300">
+      <div className="flex justify-center p-2 w-1/5 bg-green-300">
         1D/V: {collection.stats.one_day_volume?.toFixed(2)}Ξ
       </div>
     </article>
