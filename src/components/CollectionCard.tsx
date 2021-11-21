2dac5c354a1785f60f5e8300fd43da74da163663
Chris Bailey <chris@ghostly.io>
2021-11-20 21:27:52 -0500

    :sparkles: use next/image

--- src/components/CollectionCard.tsx
+++ src/components/CollectionCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import { Collection } from '../types';
 
 interface Props {
@@ -13,25 +14,27 @@ export const CollectionCard = ({ collection }: Props) => {
     >
       <div className="flex-grow p-2">
         <p className="flex items-center">
-          <img
-            src={collection.image_url}
-            height="30"
-            width="30"
-            className="mr-2 rounded border-2 border-black"
-          />
+          {collection?.image_url && (
+            <Image
+              src={collection.image_url}
+              height={30}
+              width={30}
+              className="rounded-full shadow-inner"
+            />
+          )}
           <a
             href={`https://opensea.io/collection/${collection.slug}`}
-            className="mr-2 text-blue-700 underline hover:text-blue-800 hover:no-underline"
+            className="mx-2 text-blue-700 underline hover:text-blue-800 hover:no-underline"
           >
             {collection.name}
           </a>{' '}
           ({collection.owned_asset_count})
         </p>
       </div>
-      <div className="flex justify-center p-2 w-1/5 bg-blue-300 rounded-l">
+      <div className="flex justify-center items-center p-2 w-1/5 bg-blue-300 rounded-l">
         F: {collection.stats.floor_price?.toFixed(2)}Ξ
       </div>
-      <div className="flex justify-center p-2 w-1/5 bg-green-300">
+      <div className="flex justify-center items-center p-2 w-1/5 bg-green-300">
         1D/V: {collection.stats.one_day_volume?.toFixed(2)}Ξ
       </div>
     </article>
