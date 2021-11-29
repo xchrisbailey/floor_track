7b5677c64e9d843ea136defcf7e5fa4d4e5017ee
Chris Bailey <chris@ghostly.io>
2021-11-28 22:44:17 -0500

    add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import 'tailwindcss/tailwind.css';
 import { GlobalStyles } from 'twin.macro';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Floor Track</title>
+      </Head>
       <GlobalStyles />
       <Component {...pageProps} />
     </>
